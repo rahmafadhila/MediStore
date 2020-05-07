cd1ffe1c8ab3f38625495ae26c390ecbd81c8c40
unknown <lestarisukma@gmail.com>
2020-05-07 23:57:16 +0700

    after dan catalog

---
+++ application/controllers/After.php
@@ -0,0 +1,36 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class After extends CI_Controller
+{
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	function __construct() {
+		parent::__construct();
+		$this->load->model("m_Login");
+		$this->load->model("m_product");
+	}
+
+	 public function index()
+	{
+		// $this->load->view('welcome_message');
+		$this->load->view('Users/Login/header_after');
+		$dataproduct= $this->m_product->GetProduct();
+		$this->load->view('Users/Login/Home',['dataproduct'=>$dataproduct]);
+	}
+}
+
---
+++ application/controllers/catalog.php
@@ -0,0 +1,91 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class catalog extends CI_Controller
+{
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	function __construct(){
+		parent::__construct();
+		$this->load->model("m_product");
+	}
+	public function index()
+	{
+		// $this->load->view('welcome_message');
+		// $this->load->view('Users/Template/header');
+		$dataproduct = $this->m_product->GetProduct();
+		$this->load->view('Users/Login/Home',['dataproduct'=>$dataproduct]);
+	}
+	
+	function tambahkan_cart(){ //fungsi Add To Cart
+		$data = array(
+			'id' => $this->input->post('produk_id'), 
+			'name' => $this->input->post('produk_nama'), 
+			'price' => $this->input->post('produk_harga'), 
+			'qty' => $this->input->post('jumlah'), 
+		);
+		$this->cart->insert($data);
+		echo $this->tampilkan_cart(); //tampilkan cart setelah added
+	}
+
+	function tampilkan_cart(){ //Fungsi untuk menampilkan Cart
+		$output = '';
+		$no = 0;
+		foreach ($this->cart->contents() as $items) {
+			$no++;
+			$output .='
+				<tr>
+					<td>'.$items['name'].'</td>
+					<td>'.number_format($items['price']).'</td>
+					<td>'.$items['qty'].'</td>
+					<td>'.number_format($items['subtotal']).'</td>
+					<td><button type="button" id="'.$items['rowid'].'" class="hapus_cart btn btn-danger btn-xs">Delete</button></td>
+				</tr>
+			';
+		}
+		$output .= '
+			<tr>
+				<th colspan="3">Total</th>
+				<th colspan="2">'.'Rp '.number_format($this->cart->total()).'</th>
+			</tr>
+		';
+		return $output;
+	}
+
+	function load_cart(){ //load data cart
+		echo $this->tampilkan_cart();
+	}
+
+	function clear(){
+		$data = array(
+			'rowid' => $this->input->post('row_id'), 
+			'qty' => 0, 
+		);
+		$this->cart->destroy();
+		echo $this->tampilkan_cart();
+	}
+
+	function hapus_cart(){ //fungsi untuk menghapus item cart
+		$data = array(
+			'rowid' => $this->input->post('row_id'), 
+			'qty' => 0, 
+		);
+		$this->cart->update($data);
+		echo $this->tampilkan_cart();
+	}
+}
+
